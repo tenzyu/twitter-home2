d8e0b55aa2a6a357293e80a3a74f1331ac40bcd6
Tenzyu Masuda <tenzyu.on@gmail.com>
2021-11-02 19:50:46 +0900

    add environment variable

---
+++ constant.py
@@ -0,0 +1,11 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = getenv("API_KEY")
+API_SECRET_KEY = getenv("API_SECRET_KEY")
+ACCESS_TOKEN = getenv("ACCESS_TOKEN")
+ACCESS_TOKEN_SECRET = getenv("ACCESS_TOKEN_SECRET")
+HOME2_LIST_ID = 1455276450546786306
