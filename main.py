bcb43ea500cff5987a6ec09fd24ceba6f4b019b5
Tenzyu Masuda <tenzyu.on@gmail.com>
2021-11-05 23:48:18 +0900

    refactor

--- main.py
+++ main.py
@@ -29,7 +29,6 @@ class Home2:
         self.api.remove_list_members(list_id=self.list_id, user_id=user_ids)
 
     def add_list_members(self) -> None:
-        # 過去に入れたユーザーを入れないようにする仕様
         user_ids = self.get_random_user_ids().difference(self.pickle.data)
         self.api.add_list_members(list_id=self.list_id, user_id=user_ids)
         self.update_added_user_ids(user_ids)
@@ -38,13 +37,12 @@ class Home2:
         self.pickle.data.update(user_ids)
         self.pickle.save()
 
-    # for scheduler
-    def update_home2(self) -> None:
+    def run(self) -> None:
         self.remove_all_list_members()
         self.add_list_members()
 
 
 if __name__ == "__main__":
     scheduler = BlockingScheduler({"apscheduler.timezone": "Asia/Tokyo"})
-    scheduler.add_job(Home2().update_home2, "cron", hour=0)
+    scheduler.add_job(Home2().run, "cron", hour=0)
     scheduler.start()
