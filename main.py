d998658334a131c1b5c4aeea3133952cf1739b6d
Tenzyu Masuda <tenzyu.on@gmail.com>
2021-11-05 23:42:39 +0900

    refactor main.py

--- main.py
+++ main.py
@@ -1,77 +1,47 @@
-import pickle
-
-import tweepy
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 import constant
-
-
-class MyPickle:
-    @staticmethod
-    def save(obj, filename):
-        with open(filename, "wb") as f:
-            pickle.dump(obj, f)
-
-    @staticmethod
-    def load(filename):
-        with open(filename, "rb") as f:
-            try:
-                return pickle.load(f)
-            except EOFError:
-                MyPickle.save(set(), filename)
-                return pickle.load(f)
+from modules.pickle import Pickle
+from modules.twitter import Twitter
 
 
 class Home2:
-    __slots__ = [
-        "api_key",
-        "api_secret_key",
-        "access_token",
-        "access_token_secret",
-        "api",
-        "data",
-    ]
+    __slots__ = ("api", "pickle", "list_id")
 
     def __init__(self) -> None:
-        self.api_key = constant.API_KEY
-        self.api_secret_key = constant.API_SECRET_KEY
-        self.access_token = constant.ACCESS_TOKEN
-        self.access_token_secret = constant.ACCESS_TOKEN_SECRET
-        self.api = self.get_api()
-        self.data: set[int] = MyPickle.load("data.pkl")
+        self.api = Twitter(*constant.TWITTER_CREDENTIALS)
+        self.pickle = Pickle(path="data.pkl", default_data=set[int]())
+        self.list_id = constant.HOME2_LIST_ID
 
-    def get_api(self) -> tweepy.API:
-        auth = tweepy.OAuthHandler(self.api_key, self.api_secret_key)
-        auth.set_access_token(self.access_token, self.access_token_secret)
-        return tweepy.API(auth)
+    def get_list_member_ids(self) -> set[int]:
+        count = self.api.get_list(list_id=self.list_id).member_count
+        members = self.api.get_list_members(list_id=self.list_id, count=count)
+        return set(map(lambda member: member.id, members))
 
     def get_random_user_ids(self) -> set[int]:
         tweets = self.api.search_tweets(
-            q="-filter:retweets -filter:replies",
-            count=50,
-            lang="ja",
+            "-filter:retweets -filter:replies", count=50, lang="ja"
         )
-        return {tweet.user.id for tweet in tweets}
+        return set(map(lambda tweet: tweet.user.id, tweets))
+
+    def remove_all_list_members(self) -> None:
+        user_ids = self.get_list_member_ids()
+        self.api.remove_list_members(list_id=self.list_id, user_id=user_ids)
+
+    def add_list_members(self) -> None:
+        # 過去に入れたユーザーを入れないようにする仕様
+        user_ids = self.get_random_user_ids().difference(self.pickle.data)
+        self.api.add_list_members(list_id=self.list_id, user_id=user_ids)
+        self.update_added_user_ids(user_ids)
 
     def update_added_user_ids(self, user_ids) -> None:
-        self.data.update(user_ids)
-        MyPickle.save(self.data, "data.pkl")
-
-    def add_list_members(self, list_id) -> None:
-        filtered_user_ids = self.get_random_user_ids().difference(self.data)
-        self.api.add_list_members(list_id=list_id, user_id=filtered_user_ids)
-        self.update_added_user_ids(filtered_user_ids)
-
-    def remove_all_list_members(self, list_id) -> None:
-        members = self.api.get_list_members(list_id=list_id)
-        member_ids = [member.id for member in members]
-        self.api.remove_list_members(list_id=list_id, user_id=member_ids)
+        self.pickle.data.update(user_ids)
+        self.pickle.save()
 
     # for scheduler
     def update_home2(self) -> None:
-        list_id = constant.HOME2_LIST_ID
-        self.remove_all_list_members(list_id=list_id)
-        self.add_list_members(list_id=list_id)
+        self.remove_all_list_members()
+        self.add_list_members()
 
 
 if __name__ == "__main__":
